3f70735d68afb3ff3a8f1e45582ce78a9ff9071c
Bogdan Iliescu <biliescu@bitdefender.com>
2021-11-09 12:41:40 +0200

    curs1

---
+++ CusrJava/src/curs1/Tester.java
@@ -0,0 +1,25 @@
+package curs1;
+
+public class Tester {
+	
+	String nume;
+	int ratePerHour;
+	
+	
+	
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+
+	
+	
+}
